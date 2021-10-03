6857b7cfcaa417abec25362c3a47dc9488c752e0
Matheus-B-R <matheusocarinha2015@hotmail.com>
2021-10-03 12:03:17 -0300

    1f.c atualizada

--- 1f.c
+++ 1f.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-    int A[4] = {1, 2, 3, 4};
+
+    int A[4] = {1, 2, 3, 4}, X, Y, k;
     
-    int X,Y;
+    scanf("%d %d", &X, &Y);
     
-    scanf("%d %d",&X, &Y);
-    
-    for(int x=0;x<4;x++){
-    	for(int k=0;k<4;k++){
-    	   
-    	    if( X*A[k]==Y*A[x] )
-    	    {
-    	    	printf("(%d,%d)\n",A[x],A[k]);
-    	    }
-    	    
-    	}
+    for(int x = 0; x < 4; x++){
+
+    	for(k = 0; k < 4; k++){
+
+			if(X * A[k] == Y * A[x]){
+				printf("(%d,%d)\n",A[x],A[k]);
+		}
     }
-	
 	return 0;
 }
