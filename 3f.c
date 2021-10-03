40f3c8be3dd92002cb74b81ffeba164de89a428b
Carlúcio <carlucio.barros@discente.univasf.edu.br>
2021-10-03 18:29:26 -0300

    Organização geral do código e finalização do mesmo.

--- 3f.c
+++ 3f.c
@@ -3,18 +3,17 @@
 void funcionalidadeTres(int *primeiroParOrdenado, int *segundoParOrdenado);
 
 int main(void){
-    
     int primeiroParOrdenado[2];
     int segundoParOrdenado[2];
     
-    for(int i=0;i<2;i++) {
+    for(int i = 0 ; i<2 ; i++){
             
-        if(i==0){
-            printf("Informe o %d e o %d termo do %d par ordenado: ",(i+1),(i+2),(i+1));
-                scanf("%d %d", &primeiroParOrdenado[i], &primeiroParOrdenado[i+1]);
+        if(i == 0){
+            printf("Informe o %d e o %d termo do %d par ordenado: ", (i+1), (i+2), (i+1));
+            scanf("%d %d", &primeiroParOrdenado[i], &primeiroParOrdenado[i+1]);
         }else{
-            printf("Informe o %d e o %d termo do %d par ordenado: ",(i),(i+1),(i+2));
-                scanf("%d %d", &segundoParOrdenado[i-1], &segundoParOrdenado[i]);
+            printf("Informe o %d e o %d termo do %d par ordenado: ", (i), (i+1), (i+2));
+            scanf("%d %d", &segundoParOrdenado[i-1], &segundoParOrdenado[i]);
         }
     }
    
@@ -24,11 +23,10 @@ int main(void){
 }
 
 void funcionalidadeTres(int *primeiroParOrdenado, int *segundoParOrdenado){
-
-    if( primeiroParOrdenado[0]*segundoParOrdenado[1] == primeiroParOrdenado[1] * segundoParOrdenado[0] ){
+    
+    if( primeiroParOrdenado[0] * segundoParOrdenado[1] == primeiroParOrdenado[1] * segundoParOrdenado[0] ){
         printf("true\n");
     }else{
         printf("false\n");
     }
-
 }
