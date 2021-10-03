8f4cc68b9985f5c440843f4ad0155603ec839c1e
Carlúcio <carlucio.barros@discente.univasf.edu.br>
2021-10-03 12:21:52 -0300

    Organização
    Neste commit eu fiz uma identação geral do código

--- 1f.c
+++ 1f.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
     int A[4] = {1, 2, 3, 4};
-    
     int X,Y;
     
-    scanf("%d %d",&X, &Y);
+    scanf("%d %d", &X, &Y);
+
+	printf("\n");
     
-    for(int x=0;x<4;x++){
-    	for(int k=0;k<4;k++){
-    	   
-    	    if( X*A[k]==Y*A[x] )
-    	    {
-    	    	printf("(%d,%d)\n",A[x],A[k]);
+    for( int i = 0 ; i<4 ; i++ ){
+    	for( int k = 0 ; k<4 ; k++ ){ 
+    	    if( X * A[k] == Y * A[i] ){
+    	    	printf("(%d,%d)\n", A[i],A[k]);
     	    }
-    	    
     	}
     }
+
+	printf("\n\n");
 	
 	return 0;
 }
