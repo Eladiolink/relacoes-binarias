ffeacbba61385c8f66ed31f60186e1659f4af145
Eladio Leal <eladiolink1@gmail.com>
2021-10-11 22:30:03 -0300

    Correção de bugs

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include "headers/relations.h"
 
 int main(void){
+    
+    int c[2] = {4,2};
 
-    classeDeEquivalencia(4,2);
+    classeDeEquivalencia(c);
 
     int a[2] = {1,2};
     int b[2] = {1,2};
