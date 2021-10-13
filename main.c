e265b8e822ad4b87865d7dad71af8635bb9e92bd
Eladio Leal <eladiolink1@gmail.com>
2021-10-13 16:50:46 -0300

    Funcionalidade dois

--- main.c
+++ main.c
@@ -1,16 +1,54 @@
 #include <stdio.h>
 #include "headers/relations.h"
 
+int *funcionalidadeDois(int parOdenado[][2], int tamanho);
+
+typedef struct
+{
+    int x;
+    int y;
+} par;
+
 int main(void){
 
     classeDeEquivalencia(4,2);
 
-    int a[2] = {1,2};
-    int b[2] = {1,2};
+    //mesmaClasseDeEquivalencia(a, b);
 
-    mesmaClasseDeEquivalencia(a, b);
+    // espacoQuociente();
+    int par[4][2] = {{4,2} , {2,1}};
 
-    espacoQuociente();
+    funcionalidadeDois(par, 4);
 
     return 0;
+}
+
+int *funcionalidadeDois(int parOdenado[][2], int tamanho){
+    
+    printf("São uma classe de equivalencia? \n");
+    int class[tamanho];
+
+    for (int a = 0; a < tamanho; a++){
+        int classe = 0;
+        for (int b = 0; b < tamanho; b++){
+            if (parOdenado[a][0] * parOdenado[b][1] != parOdenado[b][0] * parOdenado[a][1]){
+                classe = 1;
+            }
+        }
+
+        if (classe == 0){
+            class[a] = 1;
+        }
+        else{
+            class[a] = 0;
+        }  
+    }
+
+    for(int a=0;a<tamanho;a++){
+        if(class[a]==0){
+        printf("False\n");
+        return 0;
+        }
+    }
+        printf("True\n");
 }
