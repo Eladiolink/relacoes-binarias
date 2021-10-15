44f74cedb0715da5de76a3a0049e4715c5e1b402
Eladio Leal <eladiolink1@gmail.com>
2021-10-15 11:01:56 -0300

    finalização

--- main.c
+++ main.c
@@ -3,19 +3,5 @@
 
 int main(void){
     
-    int c[2] = {4,2};
-
-    classeDeEquivalencia(c);
-   
-    int a[2]={1,1};
-    int b[2]={2,3};
-    mesmaClasseDeEquivalencia(a, b);
-
-    espacoQuociente();
-
-    int par[4][2] = {{4,2} , {2,1}};
-
-    umaClasseDeEquivalencia(par, 2);
-
     return 0;
 }
